9d3e5ae3aa06d0e9621e54c2495020b6fa518b49
yousratareq <Yusra.tareq03@gmail.com>
2024-05-31 20:04:54 +0300

    yusra

---
+++ app/src/main/java/com/example/mobileproject/login.java
@@ -0,0 +1,55 @@
+package com.example.projectt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private CheckBox rememberMeCheckBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        rememberMeCheckBox = findViewById(R.id.remember_me);
+        Button continueButton = findViewById(R.id.continue_button);
+        TextView signUpTextView = findViewById(R.id.sign_up);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                boolean rememberMe = rememberMeCheckBox.isChecked();
+
+
+                Toast.makeText(login.this,
+                        "Email: " + email + "\nPassword: " + password + "\nRemember me: " + rememberMe,
+                        Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(login.this, BrowseCarsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(login.this, Create_Account.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
