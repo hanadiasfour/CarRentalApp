dfc29e69cb08aec674fc672889ff73b597ee88c8
hanadiasfour <hanadibyrd2003@gmail.com>
2024-06-06 00:35:09 +0300

    the filters work

--- app/src/main/java/com/example/mobileproject/BrowseCarsActivity.java
+++ app/src/main/java/com/example/mobileproject/BrowseCarsActivity.java
@@ -1,18 +1,16 @@
 package com.example.mobileproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Toast;
+
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
@@ -21,9 +19,6 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -39,10 +34,23 @@ public class BrowseCarsActivity extends AppCompatActivity {
 
     private ArrayList<CarCatalogItemModel> catalogList = new ArrayList<>();//to hold the cars which appear in catalog
     private RecyclerView CCI_RV;
-//    private EditText searchBox;
     private CCI_RecyclerViewAdapter CCI_adapter;
     private final String GET_DATA_URL = "http://10.0.2.2/api/get_all_cars_h.php";
+    private final String GET_SEARCHED_URL = "http://10.0.2.2/api/get_searched_car_h.php";
     private SearchView searchView;
+    private final int BROWSE_TO_FILTER = 121;
+//    private ProgressBar progressBar;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor edit;
+
+
+    //variables to keep track of what filters were added
+    private String location = "";
+    private String minPrice = "0";
+    private String maxPrice = "2000";
+    private String sortOrder = "";
+
 
 
 
@@ -51,14 +59,18 @@ public class BrowseCarsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_browse_cars);
+
+//        //initializing the shared pref. with the special key nad its editor
+//        sharedPreferences = getSharedPreferences(getString(R.string.SHARED_KEY), Context.MODE_PRIVATE);
+//        edit = sharedPreferences.edit();
+
         handelTabSwitch();// to handle switching the tabs
         setViews();// to initiate the view objects
         setCatalogData(GET_DATA_URL);// fill the recyclerView with all items of database
         CCI_RV.setLayoutManager(new LinearLayoutManager(this));//setting the layout of the recyclerView
 
 
-
-        // Set up SearchView listener
+        // SearchView listener for changing content of recyclerView when searching
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -78,19 +90,84 @@ public class BrowseCarsActivity extends AppCompatActivity {
     private void setViews(){
 
         CCI_RV = (RecyclerView)findViewById(R.id.CCI_RecyclerView);
-        searchView = findViewById(R.id.searchView);
+        searchView = (SearchView) findViewById(R.id.searchView);
+//        progressBar = findViewById(R.id.progressBar);
 
-//        searchBox = (EditText)findViewById(R.id.CC_searchBox);
 
     }
 
     public void openFilters(View view){
-        startActivity(new Intent(this, FilterActivity.class));
+
+        //creating and filling the bundle
+        Bundle prevFilters = new Bundle();
+        prevFilters.putString(getString(R.string.LOC),location);
+        prevFilters.putString(getString(R.string.LOW),minPrice);
+        prevFilters.putString(getString(R.string.HIGH),maxPrice);
+        prevFilters.putString(getString(R.string.ORD),sortOrder);
+
+        //creating intent
+        Intent openFilter = new Intent(this, FilterActivity.class);
+        openFilter.putExtras(prevFilters);//adding bundle to intent
+        startActivityForResult(openFilter,BROWSE_TO_FILTER);//invoking the filter activity
+
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode,int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == BROWSE_TO_FILTER && resultCode == RESULT_OK && data != null) {
+            String returnedURL = data.getStringExtra(getString(R.string.FILTER_CONSTRUCTED_URL));
+            setCatalogData(returnedURL);//setting the recyclerVIew to the filtered date
+            dissectUrlForVars(returnedURL);//assigning the variables with the set filters to save for later
+        }
+    }
+
+    public void dissectUrlForVars(String url) {
+        // removing the main first part of URL
+        String paramsLine = url.split("\\?")[1];
+
+        // splitting  by & to get the variables
+        String[] params = paramsLine.split("&");
+
+        //initializing the variables as string name "null"
+        location = "";
+        minPrice = "0";
+        maxPrice = "2000";
+        sortOrder = "";
+
+        //looping all of the coupled parameters + values to assign them to the correct variables
+        for (String param : params) {
+            String[] pair = param.split("=");//splitting couple of parameter + value by "="
+
+            String varName = pair[0];//var name
+            String value = pair.length > 1 ? pair[1] : "";//value
+
+            // assigning the values or null if they are empty
+            switch (varName) {
+                case "location":
+                    location = value.isEmpty() ? "" : value;
+                    break;
+                case "minPrice":
+                    minPrice = value.isEmpty() ? "0" : value;
+                    break;
+                case "maxPrice":
+                    maxPrice = value.isEmpty() ? "2000" : value;
+                    break;
+                case "sortOrder":
+                    sortOrder = value.isEmpty() ? "" : value;
+                    break;
+            }
+        }
 
     }
 
 
-    public void setCatalogData(String url){
+    public void fetchData(String url){
+
+//        // showing the progress bar
+//        progressBar.setVisibility(View.VISIBLE);
+
+
         catalogList = new ArrayList<>();// to hold the data for browsing
 
         JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url,
@@ -130,6 +207,32 @@ public class BrowseCarsActivity extends AppCompatActivity {
     }
 
 
+    public void setCatalogData(String url){
+
+
+        // Show the progress bar
+//        progressBar.setVisibility(View.VISIBLE);
+
+                fetchData(url);
+
+//        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+
+
+
+
+
+
+    private void performSearch(String query) {
+        if (query.isEmpty()) {
+            setCatalogData(GET_DATA_URL); // Load all data
+        } else {
+            String newURL = GET_SEARCHED_URL+"?criteria=" + query;
+            setCatalogData(newURL); // Load filtered data
+        }
+    }
+
 
 
     @Override
@@ -140,33 +243,6 @@ public class BrowseCarsActivity extends AppCompatActivity {
 
 
     }
-
-
-    private void performSearch(String query) {
-        if (query.isEmpty()) {
-            setCatalogData(GET_DATA_URL); // Load all data
-        } else {
-            String newURL = "http://10.0.2.2/api/get_searched_car_h.php?criteria=" + query;
-            setCatalogData(newURL); // Load filtered data
-        }
-    }
-
-
-    //when pressing the search button, this method fills recyclerView with refined results according to the specified criteria
-//    public void searchBtn(View view){
-//
-//        String searchCriteria = searchBox.getText().toString();//obtain the searching criteria
-//
-//        if(searchCriteria.isEmpty()){//when nothing put into the search box
-//            setCatalogData(GET_DATA_URL);//fill with all information
-//
-//        }else{//all inputs are fine
-//            String newURL = GET_DATA_URL +"?criteria=" + searchCriteria;//construct URL
-//            setCatalogData(newURL);// invoke method to obtain and display results
-//        }
-//
-//
-//    }
 
 
     @Override
--- app/src/main/java/com/example/mobileproject/FilterActivity.java
+++ app/src/main/java/com/example/mobileproject/FilterActivity.java
@@ -1,7 +1,12 @@
 package com.example.mobileproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
@@ -10,26 +15,186 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.slider.RangeSlider;
+import com.google.android.material.slider.Slider;
+
+import java.net.URLEncoder;
+import java.util.List;
+
 public class FilterActivity extends AppCompatActivity {
+
+    private Spinner locationSpinner;
+    private RadioGroup orderGroup;
+    private RadioButton ascBtn,descBtn;
+    private RangeSlider priceSlider;
+    private String GET_Filtered_URL = "http://10.0.2.2/api/get_filtered_cars_h.php";
+
+    private String[] spinnerValuesArray;
+    private String location = "";
+    private String minPrice = "0";
+    private String maxPrice = "2000";
+    private String sortOrder = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_filter);
+        setViews();//initializing the views
+        setSpinner();//initializing the spinner
+        Intent sentIntent = getIntent();//getting intent which invoked this activity
+        Bundle sentBundle = sentIntent.getExtras();//extracting the bundle in the intent
 
-        Spinner spinner = (Spinner) findViewById(R.id.location_spinner_filter);
 
-        String[] values =getResources().getStringArray(R.array.location_values);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                R.layout.spinner_item, values);
+        //getting data from bundle
+        if(sentBundle != null){
+        location = sentBundle.getString(getString(R.string.LOC));
+        minPrice = sentBundle.getString(getString(R.string.LOW));
+        maxPrice = sentBundle.getString(getString(R.string.HIGH));
+        sortOrder = sentBundle.getString(getString(R.string.ORD));
+        }
 
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        setFilterValues();//sets the values of the filters according to any previously used filters
+    }
 
+
+    //this method gives the initial value of the filters in case it was previously set
+    private void setFilterValues(){
+
+        locationSpinner.setSelection(getIndex(location));//setting location selection
+        priceSlider.setValues(Float.parseFloat(minPrice), Float.parseFloat(maxPrice));//setting slider
+
+        if(sortOrder.isEmpty()){// no order was specified
+            orderGroup.clearCheck();//clears all selections
+
+        }else if(sortOrder.equals("ASC")){//ascending was previously checked
+            orderGroup.clearCheck();//clears all selections
+            orderGroup.check(R.id.price_asc_button);
+
+        }else{//descending was previously checked
+            orderGroup.clearCheck();//clears all selections
+            orderGroup.check(R.id.price_dsc_button);
+        }
+    }
+
+
+    //given the location name, return its index in the spinner array
+    private int getIndex(String loc){
+
+        //loop the array and find the index of the matched location name
+        for(int i =0;i<spinnerValuesArray.length;i++){
+            if(spinnerValuesArray[i].equals(loc))
+                return i;//return the found index
+        }
+        return 0; //not found in spinner (impossible case)
+    }
+
+
+    //initializes the views with their ids
+    private void setViews(){
+        orderGroup = (RadioGroup) findViewById(R.id.price_sorting_group);
+        ascBtn = (RadioButton) findViewById(R.id.price_asc_button);
+        descBtn = (RadioButton)findViewById(R.id.price_dsc_button);
+        locationSpinner = (Spinner)findViewById(R.id.location_spinner_filter);
+        priceSlider =(RangeSlider)findViewById(R.id.range_slider_filter);
+
+    }
+
+
+    //on click action invoked by the apply button
+    // takes the filter inputs from the layout views and creates a url to access the api
+    public void applyFilters(View view){
+
+        ////////////////////////////////////
+        /////getting selected location/////
+        ////////////////////////////////////
+        String selectedLoc = locationSpinner.getSelectedItem().toString();
+        if(selectedLoc.equals("Select")){//default choice (nothing was selected)
+            selectedLoc="";//empty the location string
+        }
+
+        //////////////////////////////////////
+        /////getting selected price range/////
+        //////////////////////////////////////
+        //get set range from slider
+        List<Float> values = priceSlider.getValues();
+        int lowerBound = Math.round(values.get(0));
+        int upperBound = Math.round(values.get(1));
+
+
+        ///////////////////////////////////
+        /////getting selected ordering/////
+        ///////////////////////////////////
+        String order =""; //to hold DESC or ASC
+        int selectedID = orderGroup.getCheckedRadioButtonId();//get id of selected radio button
+
+        if(selectedID != -1){//something was selected
+
+            //getting the view associated with this id
+            RadioButton selectedOrder = (RadioButton) findViewById(selectedID);
+            String value =selectedOrder.getText().toString();//button label
+
+            if(value.equals(getString(R.string.price_asc_label))){//ascending
+                order = "ASC";
+
+            }else{//descending
+                order = "DESC";
+            }
+
+
+        }
+
+        //obtaining the url to invoke the backend api
+        String constructedURL = getConstructedURL(selectedLoc,lowerBound,upperBound,order);
+
+
+        //sending back result url through intent to the Browsing activity
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(getString(R.string.FILTER_CONSTRUCTED_URL), constructedURL);
+        setResult(RESULT_OK, returnIntent);
+        finish();
 
 
 
     }
+
+
+    //constructs the url to invoke the backend api to filter cars
+    private String getConstructedURL(String location,int lower,int upper, String order){
+
+        String constructedURL = GET_Filtered_URL+"?";
+        constructedURL += "location=" + location;
+        constructedURL += "&minPrice=" + lower;
+        constructedURL += "&maxPrice=" + upper +"&sortOrder=" + order;
+
+        return constructedURL;
+
+    }
+
+
+    //clears the filters set (rest all views to initial values)
+    public void clearFilters(View view){
+        locationSpinner.setSelection(0);
+        priceSlider.setValues((float) 0, (float)2000);
+        orderGroup.clearCheck();//clears all selections
+
+    }
+
+    //initializes and occupies the spinner with the values for the locations
+    private void setSpinner(){
+
+        //initializing an array with the values set in the array string resources
+        spinnerValuesArray =getResources().getStringArray(R.array.location_values);
+        // creating ArrayAdapter using the string array and a special spinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                R.layout.spinner_item, spinnerValuesArray);
+
+        // setting the adapter to the spinner
+        locationSpinner.setAdapter(adapter);
+
+    }
+
+
 }
